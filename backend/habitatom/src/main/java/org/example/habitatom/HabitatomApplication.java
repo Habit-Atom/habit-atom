a456f275cb250c57af9b65f97a53335a9ecf6063
NikolaAlchev <nikolalcev2002@gmail.com>
2024-05-13 13:38:14 +0200

    Added initial files for frontend and backend

---
+++ backend/habitatom/src/main/java/org/example/habitatom/HabitatomApplication.java
@@ -0,0 +1,13 @@
+package org.example.habitatom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabitatomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HabitatomApplication.class, args);
+    }
+
+}
---
+++ backend/habitatom/src/test/java/org/example/habitatom/HabitatomApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.habitatom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabitatomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
